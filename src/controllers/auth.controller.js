49ce01cf54dfcc742a7f98a583493b123bb5d85d
Oleh Ferents <olegferenc1@gmail.com>
2020-12-08 09:53:29 +0200

    Response header

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,10 +10,14 @@ module.exports = {
             const user = await userService.getUserByEmail(email);
             const accessToken = jwt.sign({ userId: user.id }, JWT_SECRET);
 
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
             res.send({
                 accessToken,
             });
         } else {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
             res.status(404).send({
                 message: 'User with such email is not exists!',
             });
@@ -24,11 +28,15 @@ module.exports = {
         const { name, email } = req.body;
         const isUserExists = await userService.checkIfExists(email);
         if (isUserExists) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
             res.status(412).send({
                 message: 'User with such email is already exists!',
             });
         } else {
             const user = await userService.saveUser({name, email});
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
             res.status(201).send(user);
         }
     }
