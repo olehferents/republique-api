a036ebd65c10544b71cc43bfaedfe380c1247302
Oleh Ferents <olegferenc1@gmail.com>
2020-12-08 09:49:07 +0200

    Change config

--- src/models/index.js
+++ src/models/index.js
@@ -4,10 +4,22 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 
-const {DATABASE_URI} = require('./../config');
 const db = {};
 
-const sequelize = new Sequelize(DATABASE_URI);
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    }
+});
 
 fs
     .readdirSync(__dirname)
